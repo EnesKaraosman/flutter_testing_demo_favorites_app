41f99bcb0916a660ad621d59bb79571d1bef749d
Enes Karaosman <eneskaraosman53@gmail.com>
2021-09-13 11:47:47 +0300

    [MOD] adding & removing from favorites tests coded.

--- test/models/favorites_test.dart
+++ test/models/favorites_test.dart
@@ -2,5 +2,21 @@ import 'package:test/test.dart';
 import 'package:favorites/models/favorites.dart';
 
 void main() {
+  group('Testing App Provider', () {
+    var favorites = Favorites();
 
+    test('A new item should be added', () {
+      var number = 35;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+    });
+
+    test('An item should be removed', () {
+      var number = 53;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+      favorites.remove(number);
+      expect(favorites.items.contains(number), false);
+    });
+  });
 }
